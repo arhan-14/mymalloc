f61453356385022b095ae25723e761da50040f52
arhan-14 <arhannagavelli@gmail.com>
2026-02-24 23:39:28 -0500

    added mymalloc unique allocation test

--- README.txt
+++ README.txt
@@ -1,2 +1,9 @@
 Arhan Nagavelli - arn97
 Keith Miquela - kvm33
+
+TESTING PLAN
+-------------
+mallocation.c (specifications as per writeup example)
+- Requirement: malloc() reserves unallocated memory.
+- Detection method: when successful, malloc() returns a pointer to an object that does not overlap with any other allocated object.
+- Test: Write a program that allocates three large objects. Once allocation is complete, it fills each object with a distinct byte pattern (object 1 is 0x01, object 2 is 0x02, and object 3 is 0x03). Finally, it checks whether each object still contains the written pattern.
---
+++ mallocationtest.c
@@ -0,0 +1,40 @@
+#include "mymalloc.h"
+
+typedef struct TestObject TestObject{
+    void *ptr;
+    size_t size;
+    char pattern;
+};
+
+int main(int argc, char** argv)
+{
+    void *obj1 = mymalloc(100, __FILE__, __LINE__);
+    void *obj2 = mymalloc(100, __FILE__, __LINE__);
+    void *obj3 = mymalloc(100, __FILE__, __LINE__);
+
+    memset(obj1, 0x01, 100);
+    memset(obj2, 0x02, 100);
+    memset(obj3, 0x03, 100);
+
+    TestObject one = {obj1, 100, 0x01};
+    TestObject two = {obj2, 100, 0x02};
+    TestObject three = {obj3, 100, 0x03};
+
+    TestObject objects[3] = {one, two, three};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            if (((char *)objects[i].ptr)[j] != objects[i].pattern)
+            {
+               printf("object %d failed\n", i);
+               printf("expected byte value: %d, actual: %d", objects[i].pattern, ((char *)objects[i].ptr)[j]);
+               return 1;
+            }
+        }
+    }
+    
+    printf("mallocationtest completed successfully!"); 
+    return 0;
+}
