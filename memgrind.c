2249f782cf93773d6373472521b2693732dd2473
arhan-14 <arhannagavelli@gmail.com>
2026-02-25 19:36:47 -0500

    complete task4 and task5 for memgrind

--- memgrind.c
+++ memgrind.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 #include "mymalloc.h"
 
+//for task 4
+typedef struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+} Node;
+
+//for task 5
+typedef struct TreeNode {
+    int data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
+
 void task1(void)
 {
     for (int i = 0; i < 50; i++)
@@ -68,9 +82,99 @@ void task3(void)
 
 }
 
+//linked list
 void task4()
 {
-    
+    Node *head = NULL;
+    Node *prev = NULL;
+
+    for (int i = 0; i < 50; i++)
+    {
+        Node *node = mymalloc(sizeof(Node) , __FILE__, __LINE__);
+        if (i == 0)
+        {
+            head = node;
+        }
+        node->data = i;
+        if (prev != NULL)
+        {
+            node->prev = prev;
+            prev->next = node;
+        }
+        prev = node;
+        node->next = NULL; 
+    }
+
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        Node *temp = curr->next;
+        myfree(curr, __FILE__, __LINE__);
+        curr = temp;
+    }
+}
+
+//helper functions for task 5 (binary search tree)
+TreeNode * insertNode(TreeNode *root, int value)
+{
+    if (root == NULL)
+    {
+        TreeNode *node = mymalloc(sizeof(TreeNode), __FILE__, __LINE__);
+        node->data = value;
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+    }
+    TreeNode *curr = root;
+    TreeNode *parent = NULL;
+    while (curr != NULL)
+    {
+        if (value < curr->data)
+        {
+            parent = curr;
+            curr = curr->left;
+        }
+        else
+        {
+            parent = curr;
+            curr = curr->right;
+        }
+    }
+    curr = mymalloc(sizeof(TreeNode), __FILE__, __LINE__);
+    curr->data = value;
+    curr->left = NULL;
+    curr->right = NULL;
+    if (value < parent->data)
+    {
+        parent->left = curr;
+    }
+    else
+    {
+        parent->right = curr;
+    }
+    return root;
+}
+
+void freeTree(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    freeTree(root->left);
+    freeTree(root->right);
+    myfree(root, __FILE__, __LINE__);
+}
+
+void task5()
+{
+    TreeNode *root = NULL;
+    for (int i = rand() % 1000; i < 50; i++)
+    {
+        root = insertNode(root, i);
+    }
+    freeTree(root);
+
 }
 
 int main(int argc, char** argv)
@@ -83,6 +187,8 @@ int main(int argc, char** argv)
         task1();
         task2();
         task3();
+        task4();
+        task5();
     }
     gettimeofday(&end, NULL);
     long seconds = end.tv_sec - start.tv_sec;
